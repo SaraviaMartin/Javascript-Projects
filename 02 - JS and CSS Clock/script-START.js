a0c529f177b850ff19f8fd6b2ac9390dd0d62fcc
Juan Martin Saravia Da Silva <saraviamartin16@hotmail.com>
2023-02-06 10:20:11 -0300

    no aparece el reloj en pantalla

---
+++ 02 - JS and CSS Clock/script-START.js
@@ -0,0 +1,9 @@
+function startTime(){
+    let today = new Date();
+    const hour = today.getHours();
+    const min = today.getMinutes();
+    const sec = today.getSeconds();
+
+    document.getElementById('clock').innerHTML = hour + ":" + min + ":" + sec;
+    
+}
